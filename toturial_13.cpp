891227b5ee23747643ed40b13fe5da472ebefb65
aminGiti <atashnezhad1@gmail.com>
2021-07-12 00:04:33 -0500

    tots10

---
+++ toturial_13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    string aminstr = "Hello world";
+
+    cout<<aminstr[0]<<endl;
+    for(int i; i< aminstr.length(); i++){
+        cout<<aminstr[i]<<endl;
+
+    }
+
+
+
+}
