b7b3f2a5a6b61d1195db22b7dd7ab36dcb7a073e
aminGiti <atashnezhad1@gmail.com>
2021-07-28 23:21:13 -0500

    newfunc

--- toturial_new_1.cpp
+++ toturial_new_1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include"amin_functions.h"
 using namespace std;
 
 
@@ -17,6 +17,7 @@ cout << "amin function works 123" << endl;
 int main(){
 
     amin_func();
+    cout << "amin function was called\nfrom another file = \n" <<there_is_no(4, 6)<< endl;
 
 
 
