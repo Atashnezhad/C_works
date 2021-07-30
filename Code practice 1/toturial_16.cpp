c2b4cea7b4d632263ebe453a7c432ecfe5550e30
Atash <atashnezhad1@gmail.com>
2021-07-29 19:40:35 -0500

    toturial_16

--- Code practice 1/toturial_16.cpp
+++ Code practice 1/toturial_16.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <tuple>
-#include<string>
+// #include<string>
 using namespace std;
 
 int main(){
 
-    tuple <int, string> aminsdw(20,"ADAdadad");
-    cout << get<0>(aminsdw);
+    tuple <int, string> person(20,"amin atashnezhad"); // this how to initialize the values and name of tuple
+
+    cout << get<1>(person) << endl; //and notice that the 0 means the first element and 1 means the second element and so on!
+    // if you gonna change an element of tuple check out below
+    cout << "\n\n" << endl;
+    get<1>(person) = "billy";    
+    cout << get<1>(person) << endl;
+
+
+    // we can also initialize tuple in differnet line
+
+
+
+
+
+
+
 
 
 }
