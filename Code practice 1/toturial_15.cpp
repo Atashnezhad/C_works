3a0f3624facef4b872d24aa1dafcd6660c7df61f
Atash <atashnezhad1@gmail.com>
2021-07-29 19:23:56 -0500

    pointer and references

--- Code practice 1/toturial_15.cpp
+++ Code practice 1/toturial_15.cpp
@@ -5,11 +5,14 @@ using namespace std;
 int main(){
 
     int x = 4;
+    // string y2 = &x;
     int *y = &x;
 
-    cout << "\n &x location of x is "<<&x<<endl;
-    cout << "y is "<<y<<endl;
-    cout << "&y location of y is " <<&y<<endl;
-    cout << " *y value of y is " <<*y<<endl;
+    cout << "\n\n\nvalue of x is " << x << endl;
+    // cout << " \nlocation of y2 is "<< y2 << endl;
+    cout << " \nlocation of x is "<< &x << endl;
+    cout << " \nvalue of y is "<< y << endl;
+    cout << " \nlocation of y is " << &y << endl;
+    cout << " \nvalue of *y is \n\n\n" << *y << endl;
 }
 
