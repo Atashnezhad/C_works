0b6e506fdf1047f327e1777112dbfc449f1c6c18
aminGiti <atashnezhad1@gmail.com>
2021-07-31 23:57:06 -0500

    time

--- Code practice 2/prac2.cpp
+++ Code practice 2/prac2.cpp
@@ -3,31 +3,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-// #include <chrono>
-// #include <sys/time.h>
-// #include <ctime>
+
 
 using namespace std;
 
 void my_long_operation(){
     
         for(int i = 0; i<1000000; i++){
-        cout<<"value of is = "<< i <<endl;
+        // cout<<"value of is = "<< i <<endl;
         
     }
 };
 
-// int main() {
-//     struct timeval time_now;
-//     gettimeofday(&time_now, nullptr);
-//     time_t msecs_time = (time_now.tv_sec * 1000) + (time_now.tv_usec / 1000);
-//     cout << "seconds since epoch: " << time_now.tv_sec << endl;
-//     cout << "milliseconds since epoch: "  << msecs_time << endl << endl;
-//     return EXIT_SUCCESS;
-// }
+void print_string(int j, string i){
+    for (int k = 0;k<j; k++){
+        cout << i << endl;
+    }
+    
+}
+void print_double_number(double i){
+    cout << i << endl;
+}
+
 
 
 int main(){
+    print_string(4, "------");
+    system("ls -lrt"); 
+    print_string(4, "------");
+    print_string(1, "amin");
+    print_double_number(323424);
+    print_string(4, "------");
+
+   
+
     // declaring argument of time()
     time_t start_time = time(NULL);
     my_long_operation();
