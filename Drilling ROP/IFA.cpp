f9939e8d44d72b20a34a4d85530b358a97506574
Atash <atashnezhad1@gmail.com>
2021-08-05 15:48:55 -0500

    someworks

--- Drilling ROP/IFA.cpp
+++ Drilling ROP/IFA.cpp
@@ -6,6 +6,9 @@ using namespace std;
 // to compile this file simply copy and paste following in the cmd
 // clear; g++ IFA.cpp -o IFA; ./IFA
 
+// run the followinig in cmd to update git repo
+// git add .; git commit -m "someworks"; git psuh; git status
+
 void print_line_space_with_lenght(int j = 10){
     cout<<endl;
     for (int i=0;i<j;i++){ printf("=");}
