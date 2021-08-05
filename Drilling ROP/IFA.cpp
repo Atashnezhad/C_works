4cc634b83180bf3f0a9c1d5ce5a7e6682363712a
Atash <atashnezhad1@gmail.com>
2021-08-05 15:48:34 -0500

    someworks

---
+++ Drilling ROP/IFA.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+// #include <tuple>
+#include<string>
+using namespace std;
+
+// to compile this file simply copy and paste following in the cmd
+// clear; g++ IFA.cpp -o IFA; ./IFA
+
+void print_line_space_with_lenght(int j = 10){
+    cout<<endl;
+    for (int i=0;i<j;i++){ printf("=");}
+}
+
+
+
+int main()
+{
+
+    printf("This is OOP for interfacial friction angle calculations\n"
+            "the main function was developed in the python language.\n");
+            // "=======================================================\n"); cout<<endl;
+
+    print_line_space_with_lenght(75);
+
+
+
+}
