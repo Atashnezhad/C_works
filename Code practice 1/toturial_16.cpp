536d0ad1e6dd052c67c9bed175b5c08f61739455
Atash <atashnezhad1@gmail.com>
2021-08-05 13:59:37 -0500

    some works in the loop while printf

--- Code practice 1/toturial_16.cpp
+++ Code practice 1/toturial_16.cpp
@@ -5,17 +5,83 @@ using namespace std;
 
 int main(){
 
-    // typename person;
-    tuple<int, string> person(20,"amin atashnezhad");
-    
 
-    cout << get<1>(person) << endl; 
+
+    int i = 0;
+    while (i < 5) {
+    // cout << i << "\n";
+    printf("this is counter in while = %i", i); cout<<endl;
+    i++;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // typename person;
+    // tuple<int, string> person(20,"amin atashnezhad");
+    // cout << "\n"<< get<0>(person) << " \n "<<endl; 
+    // cout << "\n"<< get<1>(person) << " \n "<<endl; 
+
+
+
+
     // cout << "\n\n" << endl;
     // get<1>(person) = "billy";    
     // cout << get<1>(person) << endl;
-
-
     // we can also initialize tuple in differnet line
+
+
+
+
+    // cout <<"This is for loop in C++\n";
+    // double j;
+    // for (int i = 0; i<15; i++){ 
+
+    //     // cout << i << endl; 
+    //     printf("this is the number counter integer with 10 spaces = %10d",i); cout << endl;
+    //     j = i;
+    //     printf("this is the number counter float with 10 spaces = %10.2f",j); cout << endl;
+    // }
+    // cout << "\n" << endl;
+
+
+
+
+
+    // cout <<"This is printf in C++\n";
+    // printf("adasdasda =%10i", 2);
+
+
+    // printf ("Characters: %c %c \n", 'a', 65);
+    // printf ("Decimals: %d %ld\n", 1977, 650000L);
+    // printf ("Preceding with blanks: %10d \n", 1977);
+    // printf ("Preceding with zeros: %010d \n", 1977);
+    // printf ("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
+    // printf ("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
+    // printf ("Width trick: %*d \n", 5, 10);
+    // printf ("%s \n", "A string");
+
 
 
 }
